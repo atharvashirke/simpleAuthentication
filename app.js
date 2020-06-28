cf36a078f7d830b73344aad40222a0485d9db40a
Atharva Shirke <atharvashirke@berkeley.edu>
2020-06-28 03:37:07 -0700

    Added authentication features

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 
 app.set('view engine', 'ejs')
 
@@ -14,13 +15,31 @@ app.get('/', (req, res) => {
     res.render('register')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    var user = users.find(user => user.email === req.body.email)
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send("Hi " + user.name)
+    } else {
+        res.send("Wrong password, try again.")
+    }
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
 app.post('/register', (req, res) => {
     users.push({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: bcrypt.hashSync(req.body.password, 10)
     })
     console.log(users)
+    res.redirect('/login')
 })
 
 app.listen('3000', () => {
